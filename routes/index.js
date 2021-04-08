699fede241b133affca627b2e9906a42cfc1e682
emilsharier <emilsharier@gmail.com>
2021-04-08 22:12:23 +0530

    Adding keep me alive route for cron

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ const rootRoute = require("./root");
 
 module.exports = (app) => {
   app.use(`/api/${API_VERSION}`, rootRoute);
+  app.get("/keepMeAlive", (req, res, next) => {
+    res.sendStatus(200);
+  });
 };
